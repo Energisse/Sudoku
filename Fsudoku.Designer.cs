5dca67fd137ec8827ae4d82af2060b00e88076d3
Thomas <Thomas@DESKTOP-5HSHBPT>
2022-10-27 21:31:41 +0200

    Ajoutez des fichiers projet.

---
+++ Fsudoku.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Sodoku
+{
+    partial class Fsodoku
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dvg_motus = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dvg_motus)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dvg_motus
+            // 
+            this.dvg_motus.AllowUserToAddRows = false;
+            this.dvg_motus.AllowUserToDeleteRows = false;
+            this.dvg_motus.AllowUserToResizeColumns = false;
+            this.dvg_motus.AllowUserToResizeRows = false;
+            this.dvg_motus.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvg_motus.ColumnHeadersVisible = false;
+            this.dvg_motus.Location = new System.Drawing.Point(174, 97);
+            this.dvg_motus.Name = "dvg_motus";
+            this.dvg_motus.ReadOnly = true;
+            this.dvg_motus.RowHeadersVisible = false;
+            this.dvg_motus.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dvg_motus.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dvg_motus.Size = new System.Drawing.Size(450, 450);
+            this.dvg_motus.TabIndex = 0;
+            this.dvg_motus.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dvg_motus_CellContentClick);
+            // 
+            // Fsodoku
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 639);
+            this.Controls.Add(this.dvg_motus);
+            this.Name = "Fsodoku";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.WindowsSodoku_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dvg_motus)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dvg_motus;
+    }
+}
+
---
+++ Fsudoku.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sodoku
+{
+    public partial class Fsodoku : Form
+    {
+        int[,] grid = new int[9, 9];
+        public Fsodoku()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowsSodoku_Load(object sender, EventArgs e)
+        {
+            dvg_motus.RowCount = 9;
+            dvg_motus.ColumnCount = 9;
+            for (int x = 0; x < 9; x++)
+            {
+                dvg_motus.Columns[x].Width = 50;
+                for (int y = 0; y < 9; y++)
+                {
+                    dvg_motus.Rows[y].Height = 50;
+
+                }
+            }
+
+            this.solve(grid, 0,0);
+            for(int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    dvg_motus.Rows[y].Cells[x].Value = grid[x, y];
+                }
+            }
+        }
+        private bool solve(int[,] grid,int x, int y) 
+        {
+            if (y == 9) return true;
+            if (x == 9) return solve(grid,0, y + 1);
+            if (grid[x,y] != 0) return solve(grid,x + 1,y);
+            foreach (int i in nombreAleatoire())
+            {
+                System.Diagnostics.Debug.WriteLine("x=" + x + " y=" + y + " i= " + i + "   " + (nestPasDansLaLigne(i, x, y) && nestPasDansLaColonne(i, x, y) && controleInterieurCarre(i, x, y)));
+                if (nestPasDansLaLigne(i, x, y) && nestPasDansLaColonne(i, x, y)  && controleInterieurCarre(i, x, y))
+                {
+                    grid[x, y] = i;
+                    if (solve(grid,x + 1, y)) return true;
+                    grid[x, y] = 0;
+                }
+            }
+            return false;
+        }
+
+
+        private bool nestPasDansLaLigne(int valeur,int x, int y)
+        {
+            for(int i = 0; i < 9; i++)
+            {
+                if (grid[i, y] == valeur) return false;
+            }
+            return true;
+        }
+
+        private bool nestPasDansLaColonne(int valeur, int x, int y)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (grid[x, i] == valeur) return false;
+            }
+            return true;
+        }
+
+        private bool controleInterieurCarre(int valeur,int x, int y)
+        {
+            int colonneAbsolue = ((int)x / 3) * 3;
+            int ligneAbsolue = ((int)y / 3) * 3;
+            for ( int colonne = colonneAbsolue; colonne < colonneAbsolue + 3; colonne++)
+            {
+                for (int ligne = ligneAbsolue; ligne < ligneAbsolue + 3; ligne++)
+                {
+                    if (grid[colonne, ligne] == valeur) return false;
+                }
+            }
+            return true;
+        }
+        public static int[] nombreAleatoire()
+        {
+            int[] nombres = new int[9] {1,2,3,4,5,6,7,8,9};
+            Random rnd = new Random();
+
+            for (int i = 8; i>=0; i--)
+            {
+                int k = rnd.Next(0,8);
+                int value = nombres[k];
+                nombres[k] = nombres[i];
+                nombres[i] = value;
+            }
+            return nombres;
+         
+        }
+        private void dvg_motus_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
